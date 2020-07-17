6cf1fd07b108b1c08e357e0d026c7e0e07dc1989
Chester Guinto <chesterguinto@gmail.com>
2020-07-17 16:52:04 -0700

    test with echoes.

--- test.php
+++ test.php
@@ -1,3 +1,9 @@
 <?php
-  $dbconn = pg_connect(getenv("DATABASE_URL"))
-    or die('could not connect: ' . pg_last_error());
+$dbconn = pg_connect(getenv("DATABASE_URL"))
+  or die('could not connect: ' . pg_last_error());
+
+if ($dbconn) {
+  echo 'yes';
+} else {
+  echo 'damn';
+}
